cf4a914f5f4df6835a2cd5a69501cc941dcc3a7e
Горан Китић <kiticgoran90@gmail.com>
2024-10-17 00:33:45 +0200

    Cabins filter added

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -54,7 +54,7 @@ const Button = ({ filter, handleFilter, activeFilter, children }) => {
     return (
         <button
             onClick={() => handleFilter(filter)}
-            className={`uppercase tracking-wide px-5 py-2 hover:bg-primary-700 ${filter === activeFilter ? "bg-primary-800 text-primary-100" : ""}`}
+            className={`uppercase tracking-wide px-4 py-2 hover:bg-primary-700 ${filter === activeFilter ? "bg-primary-800 text-primary-100" : ""}`}
         >
             {children}
         </button>
